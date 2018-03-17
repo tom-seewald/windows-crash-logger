9aa5516043300da9b92a4ff6a1387242de901ecc
Tom Seewald <tseewald@gmail.com>
2018-03-17 17:00:42 -0500

    Beta09 - added sleepdiagnostics.html

--- elevated.ps1
+++ elevated.ps1
@@ -9,31 +9,46 @@ Param(
 	$Path
 )
 
+# Detect Windows version, convert the value from a string to a decimal
+$MajorVer = [System.Environment]::OSVersion.Version.Major
+$MinorVer = [System.Environment]::OSVersion.Version.Minor
+$WindowsVersion = "$MajorVer" + "." + "$MinorVer" -as [decimal]
+
 # This is set because $PSScriptRoot is not available on stock Windows 7 SP1
 $ScriptPath = Split-Path $MyInvocation.MyCommand.Path -Parent
 
 # Set output files for logging
-$Log = "$env:TEMP\script-log-elevated.csv"
-$ErrorFile = "$env:TEMP\error-temp-elevated.txt"
-New-Item -ItemType File -Path $ErrorFile -Force -ErrorAction Stop > $null
+$Log = Join-Path -Path "$env:TEMP" -ChildPath "script-log-elevated.csv"
+$ErrorFile = Join-Path -Path "$env:TEMP" -ChildPath "error-temp-elevated.txt"
+$CrashDumps = Join-Path -Path $Path -ChildPath "Crash Dumps"
+$PowerReports = Join-Path -Path $Path -ChildPath "Power Reports"
+$WER = Join-Path -Path $Path -ChildPath "Error Reports"
+$LoggerModule = Join-Path -Path $ScriptPath -ChildPath "logger-module.psm1"
+
+# Create folders
+New-Item -ItemType Directory $CrashDumps -Force -ErrorAction Stop | Out-Null
+New-Item -ItemType Directory $WER -Force -ErrorAction Stop | Out-Null
+
+# Create file for temporarily storing errors
+New-Item -ItemType File -Path $ErrorFile -Force -ErrorAction Stop | Out-Null
 
 If ( Test-Path -Path $Log ) {
 
-	Remove-Item -Force $Log
+	Remove-Item -Force $Log | Out-Null
 }
 
 # Import custom module containing support functions
 Try {
 
-    Import-Module "$ScriptPath\logger-module.psm1"
+    Import-Module $LoggerModule
 }
 
 Catch {
 
-	Write-Warning "Could not import $ScriptPath\test-module.psm1, exiting script."
+	Write-Warning "Could not import $LoggerModule, exiting script."
 	$TimeStamp = (Get-Date).ToString("yyyy/MM/dd HH:mm:ss")
-    $ImportError =  $TimeStamp + "," + "Failed to import $ScriptPath\test-module.psm1, exiting script."
-    Write-Output $ImportError >> $Log
+    $ImportError =  $TimeStamp + "," + "Failed to import $LoggerModule, exiting script."
+    Write-Output $ImportError | Out-File -Append -FilePath $Log
     Exit
 }
 
@@ -44,22 +59,17 @@ If ( $ElevatedCheck -ne "True" ) {
 
 	Write-Warning "ERROR: Administrator rights are required for this script to work properly!"
 	Write-Warning "Aborting script!"
-	Write-Log -Message "Administrator rights are required for this script to work properly, exiting script." $Log
+	Write-Log -Message "Administrator rights are required for this script to work properly, exiting script." -LogPath $Log
 	Exit
 }
-
-# Detect Windows version, convert the value from a string to a decimal
-$MajorVer=[System.Environment]::OSVersion.Version.Major
-$MinorVer=[System.Environment]::OSVersion.Version.Minor
-$VerNum = "$MajorVer" + "." + "$MinorVer" -as [decimal]
 
 # Set window size to 1000 by 1000 to avoid truncation when sending output to files
 $Host.UI.RawUI.BufferSize = New-Object Management.Automation.Host.Size (1000,1000)
 
 # Get crash dump settings and append crash dump type matrix
 Write-Host "Getting crash dump settings..."
-Write-Output "########################## Crash Dump Settings #########################" > "$Path\Crash Dumps\crash-dump-settings.txt"
-Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\CrashControl" >> "$Path\Crash Dumps\crash-dump-settings.txt" -ErrorAction SilentlyContinue -ErrorVariable ScriptError
+Write-Output "########################## Crash Dump Settings #########################" | Out-File -FilePath "$CrashDumps\crash-dump-settings.txt"
+Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\CrashControl" | Out-File -Append -FilePath "$CrashDumps\crash-dump-settings.txt" -ErrorAction SilentlyContinue -ErrorVariable ScriptError
 Write-Log -Message $ScriptError -LogPath $Log
 
 Write-Output "######################## Crash Dump Type Matrix ########################
@@ -71,7 +81,7 @@ Complete	1					<does not exist>
 Active		1					1
 Kernel		2					<does not exist>
 Small		3					<does not exist>
-Automatic	7					<does not exist>" >> "$Path\Crash Dumps\crash-dump-settings.txt"
+Automatic	7					<does not exist>" | Out-File -Append -FilePath "$CrashDumps\crash-dump-settings.txt"
 
 # Copy mini crash dumps
 $MinidumpPath = (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\CrashControl").MinidumpDir
@@ -81,7 +91,7 @@ If ( $DefaultPath -eq $MinidumpPath ) {
 
 	If ( Test-Path -Path $MinidumpPath ) {
 
-		Get-ChildItem $MinidumpPath | Sort-Object LastWriteTime -Descending >> "$Path\Crash Dumps\mini-crash-dumps.txt"
+		Get-ChildItem $MinidumpPath | Sort-Object LastWriteTime -Descending | Out-File -Append -FilePath "$CrashDumps\mini-crash-dumps.txt"
 
 		If ( $(Get-ChildItem -Filter "*.dmp" -Path "$MinidumpPath") -ne $null ) {
 
@@ -93,14 +103,14 @@ If ( $DefaultPath -eq $MinidumpPath ) {
 		Else {
 
 			Write-Host "No crash dumps to copy from $MinidumpPath"
-			Write-Output "$MinidumpPath contains no dump files." >> "$Path\Crash Dumps\mini-crash-dumps.txt"
+			Write-Output "$MinidumpPath contains no dump files." Out-File -Append -FilePath "$CrashDumps\mini-crash-dumps.txt"
 		}
 	}
 
 	Else {
 
 		Write-Host "No crash dumps to copy from $MinidumpPath"
-		Write-Output "$MinidumpPath does not exist." >> "$Path\Crash Dumps\mini-crash-dumps.txt"
+		Write-Output "$MinidumpPath does not exist." | Out-File -Append -FilePath "$CrashDumps\mini-crash-dumps.txt"
 	}
 }
 
@@ -110,7 +120,7 @@ Else {
 	# Check that the registry path exists and is not empty, copy the 5 newest crash dumps
 	If ( Test-Path -Path $MinidumpPath ) {
 
-		Get-ChildItem $MinidumpPath | Sort-Object LastWriteTime -Descending >> "$Path\Crash Dumps\mini-crash-dumps.txt"
+		Get-ChildItem $MinidumpPath | Sort-Object LastWriteTime -Descending | Out-File -Append -FilePath "$CrashDumps\mini-crash-dumps.txt"
 
 		If ( $(Get-ChildItem -Filter "*.dmp" -Path "$MinidumpPath") -ne $null ) {
 
@@ -122,19 +132,19 @@ Else {
 		Else {
 
 			Write-Host "No crash dumps to copy from $MinidumpPath"
-			Write-Output "$MinidumpPath contains no dump files." >> "$Path\Crash Dumps\mini-crash-dumps.txt"
+			Write-Output "$MinidumpPath contains no dump files." | Out-File -Append -FilePath "$CrashDumps\mini-crash-dumps.txt"
 		}
 	}
 
 	Else {
 
 		Write-Host "No crash dumps to copy from $MinidumpPath"
-		Write-Output "$MinidumpPath does not exist." >> "$Path\Crash Dumps\mini-crash-dumps.txt"
+		Write-Output "$MinidumpPath does not exist." | Out-File -Append -FilePath "$CrashDumps\mini-crash-dumps.txt"
 	}
 
 	If ( Test-Path -Path $DefaultPath ) {
 
-		Get-ChildItem $DefaultPath | Sort-Object LastWriteTime -Descending >> "$Path\Crash Dumps\mini-crash-dumps.txt"
+		Get-ChildItem $DefaultPath | Sort-Object LastWriteTime -Descending | Out-File -Append -FilePath "$CrashDumps\mini-crash-dumps.txt"
 
 		If ( $(Get-ChildItem -Filter "*.dmp" -Path "$DefaultPath") -ne $null ) {
 
@@ -146,14 +156,14 @@ Else {
 		Else {
 		
 			Write-Host "No crash dumps to copy from $DefaultPath"
-			Write-Output "$DefaultPath contains no dump files." >> "$Path\Crash Dumps\mini-crash-dumps.txt"
+			Write-Output "$DefaultPath contains no dump files." | Out-File -Append -FilePath "$CrashDumps\mini-crash-dumps.txt"
 		}
 	}
 
 	Else {
 
 		Write-Host "No crash dumps to copy from $DefaultPath"
-		Write-Output "$DefaultPath does not exist." >> "$Path\Crash Dumps\mini-crash-dumps.txt"
+		Write-Output "$DefaultPath does not exist." | Out-File -Append -FilePath "$CrashDumps\mini-crash-dumps.txt"
 	}
 }
 
@@ -164,14 +174,14 @@ If ( $DumpPath -eq "$env:SystemRoot\Memory.dmp" ) {
 
 	If ( Test-Path $DumpPath ) {
 
-		Write-Output "Crash dump found at $DumpPath" > "$Path\Crash Dumps\memory-dumps.txt"
-		Write-Output "Creation date: $((Get-Item "$DumpPath").LastWriteTime)" >> "$Path\Crash Dumps\memory-dumps.txt"
-		Write-Output "Size on disk: $([math]::truncate((Get-Item $DumpPath).Length / 1MB)) MB" >> "$Path\Crash Dumps\memory-dumps.txt"
+		Write-Output "Crash dump found at $DumpPath" | Out-File -FilePath "$CrashDumps\memory-dumps.txt"
+		Write-Output "Creation date: $((Get-Item "$DumpPath").LastWriteTime)" | Out-File -Append -FilePath "$CrashDumps\memory-dumps.txt"
+		Write-Output "Size on disk: $([math]::truncate((Get-Item $DumpPath).Length / 1MB)) MB" | Out-File -Append -FilePath "$CrashDumps\memory-dumps.txt"
 	}
 
 	Else {
 
-		Write-Output "$DumpPath was not found" >> "$Path\Crash Dumps\memory-dumps.txt"
+		Write-Output "$DumpPath was not found" | Out-File -Append -FilePath "$CrashDumps\memory-dumps.txt"
 	}
 }
 
@@ -179,26 +189,26 @@ Else {
 
 	If ( Test-Path $DumpPath ) {
 
-		Write-Output "Crash dump found at $DumpPath" > "$Path\Crash Dumps\memory-dumps.txt"
-		Write-Output "Creation date: $((Get-Item "$DumpPath").LastWriteTime)" >> "$Path\Crash Dumps\memory-dumps.txt"
-		Write-Output "Size on disk: $([math]::truncate((Get-Item $DumpPath).Length / 1MB)) MB" >> "$Path\Crash Dumps\memory-dumps.txt"
+		Write-Output "Crash dump found at $DumpPath" | Out-File -FilePath "$CrashDumps\memory-dumps.txt"
+		Write-Output "Creation date: $((Get-Item "$DumpPath").LastWriteTime)" | Out-File -Append -FilePath "$CrashDumps\memory-dumps.txt"
+		Write-Output "Size on disk: $([math]::truncate((Get-Item $DumpPath).Length / 1MB)) MB" | Out-File -Append -FilePath "$CrashDumps\memory-dumps.txt"
 	}
 
 	Else {
 
-		Write-Output "$DumpPath was not found" >> "$Path\Crash Dumps\memory-dumps.txt"
+		Write-Output "$DumpPath was not found" | Out-File -Append -FilePath "$CrashDumps\memory-dumps.txt"
 	}
 
 	If ( Test-Path "$env:SystemRoot\Memory.dmp" ) {
 
-		Write-Output "Crash dump found at $env:SystemRoot\Memory.dmp" >> "$Path\Crash Dumps\memory-dumps.txt"
-		Write-Output "Creation date: $((Get-Item $env:SystemRoot\MEMORY.DMP).LastWriteTime)" >> "$Path\Crash Dumps\memory-dumps.txt"
-		Write-Output "Size on disk: $([math]::truncate((Get-Item "$env:SystemRoot\Memory.dmp").Length / 1MB)) MB" >> "$Path\Crash Dumps\memory-dumps.txt"
+		Write-Output "Crash dump found at $env:SystemRoot\Memory.dmp" | Out-File -Append -FilePath "$CrashDumps\memory-dumps.txt"
+		Write-Output "Creation date: $((Get-Item $env:SystemRoot\MEMORY.DMP).LastWriteTime)" | Out-File -Append -FilePath "$CrashDumps\memory-dumps.txt"
+		Write-Output "Size on disk: $([math]::truncate((Get-Item "$env:SystemRoot\Memory.dmp").Length / 1MB)) MB" | Out-File -Append -FilePath "$CrashDumps\memory-dumps.txt"
 	}
 
 	Else {
 
-		Write-Output "$env:SystemRoot\Memory.dmp was not found" >> "$Path\Crash Dumps\memory-dumps.txt"
+		Write-Output "$env:SystemRoot\Memory.dmp was not found" | Out-File -Append -FilePath "$CrashDumps\memory-dumps.txt"
 	}
 }
 
@@ -206,26 +216,31 @@ Else {
 If ( $(Test-Path -Path "$env:SystemRoot\LiveKernelReports") -eq $True -and $(Get-ChildItem -Path "$env:SystemRoot\LiveKernelReports" ) -ne $null ) {
 
 	$LengthMB = @{Name="Size (MB)";Expression={[math]::Round($_.Length / 1MB, 2)}}
-	Get-ChildItem -Path "$env:SystemRoot\LiveKernelReports" -Recurse -ErrorAction SilentlyContinue -ErrorVariable ScriptError | Select-Object Name,LastWriteTime,$LengthMB > "$Path\Crash Dumps\live-kernel-reports.txt"
+	Get-ChildItem -Path "$env:SystemRoot\LiveKernelReports" -Recurse -ErrorAction SilentlyContinue -ErrorVariable ScriptError | Select-Object Name,LastWriteTime,$LengthMB | Out-File -FilePath "$CrashDumps\live-kernel-reports.txt"
 	Write-Log -Message $ScriptError -LogPath $Log
 }
 
 # Gather a System Power Report, only supported on 8.1 and newer
-If ( $VerNum -ge "6.3" ) {
+If ( $WindowsVersion -ge "6.3" ) {
 
 	Write-Host "Running system power report..."
-	&"$env:SystemRoot\System32\powercfg.exe" /sleepstudy /output "$Path\power-report.html" > $null 2> $ErrorFile
+	&"$env:SystemRoot\System32\powercfg.exe" /sleepstudy /output "$PowerReports\power-report.html" 2> $ErrorFile | Out-Null
 	Write-CommandError -ErrorFile $ErrorFile -LogPath $Log
 }
 
-# Disk and partition information, 8.1 requires admin rights unlike 10
-If ( $VerNum -eq "6.3" ) {
+# Run a sleep diagnostics report
+Write-Host "Running sleep diagostics..."
+&"$env:SystemRoot\System32\powercfg.exe" /systemsleepdiagnostics /output "$PowerReports\sleep-diagnostics.html" 2> $ErrorFile | Out-Null
+Write-CommandError -ErrorFile $ErrorFile -LogPath $Log
 
-	Get-Partition -ErrorAction SilentlyContinue -ErrorVariable ScriptError | Format-List >> "$Path\partitions.txt"
+# Disk and partition information, 8.1 requires admin rights unlike 10
+If ( $WindowsVersion -eq "6.3" ) {
+
+	Get-Partition -ErrorAction SilentlyContinue -ErrorVariable ScriptError | Format-List | Out-File -Append -FilePath "$Path\partitions.txt"
 	Write-Log -Message $ScriptError -LogPath $Log
 	
 	$DiskAttributes = "FriendlyName", "Model", "Manufacturer", "Number", "IsBoot", "AllocatedSize", "HealthStatus", "OperationalStatus", "BusType", "FirmwareVersion", "PartitionStyle", "Path"
-	Get-Disk -ErrorAction SilentlyContinue -ErrorVariable ScriptError | Select-Object $DiskAttributes | Format-List >> "$Path\disks.txt"
+	Get-Disk -ErrorAction SilentlyContinue -ErrorVariable ScriptError | Select-Object $DiskAttributes | Format-List | Out-File -Append -FilePath "$Path\disks.txt"
 	Write-Log -Message $ScriptError -LogPath $Log
 }
 
@@ -233,30 +248,30 @@ If ( $VerNum -eq "6.3" ) {
 Write-Host "Listing PnP devices..."
 
 $DriverAttributes = "Name", "Status", "ConfigManagerErrorCode", "Description", "Manufacturer", "DeviceID"
-Get-WmiObject Win32_PNPEntity -ErrorAction SilentlyContinue -ErrorVariable ScriptError | Select-Object $DriverAttributes | Sort-Object Name | Format-Table -AutoSize >> "$Path\pnp-devices.txt"
+Get-WmiObject Win32_PNPEntity -ErrorAction SilentlyContinue -ErrorVariable ScriptError | Select-Object $DriverAttributes | Sort-Object Name | Format-Table -AutoSize | Out-File -Append -FilePath "$Path\pnp-devices.txt"
 Write-Log -Message $ScriptError -LogPath $Log
 
 # List all processes
 Write-Host "Enumerating running processes..."
 
 $ProcessAttributes = "ProcessName", "ProcessID", "SessionId", "Priority", "CommandLine"
-Get-WmiObject Win32_Process -ErrorAction SilentlyContinue -ErrorVariable ScriptError | Select-Object $ProcessAttributes | Sort-Object ProcessName,ProcessId | Format-Table -AutoSize > "$Path\processes.txt"
+Get-WmiObject Win32_Process -ErrorAction SilentlyContinue -ErrorVariable ScriptError | Select-Object $ProcessAttributes | Sort-Object ProcessName,ProcessId | Format-Table -AutoSize | Out-File -FilePath "$Path\processes.txt"
 Write-Log -Message $ScriptError -LogPath $Log
 
 # List all services including status, pid, only Windows 10 has support for listing service StartType via Get-Service
 Write-Host "Identifying running services..."
 
-If ( $VerNum -ge "10.0" ) {
+If ( $WindowsVersion -ge "10.0" ) {
 
 	$StartType = @{Name="StartType";Expression={(Get-Service $_.Name).StartType}}
 	
-	Get-WmiObject Win32_Service -ErrorAction SilentlyContinue -ErrorVariable ScriptError | Select-Object Name, DisplayName, State, ProcessID, $StartType | Sort-Object State, Name | Format-Table -AutoSize > "$Path\services.txt"
+	Get-WmiObject Win32_Service -ErrorAction SilentlyContinue -ErrorVariable ScriptError | Select-Object Name, DisplayName, State, ProcessID, $StartType | Sort-Object State, Name | Format-Table -AutoSize | Out-File -FilePath "$Path\services.txt"
 	Write-Log -Message $ScriptError -LogPath $Log	
 }
 
 Else {
 
-	Get-WmiObject Win32_Service -ErrorAction SilentlyContinue -ErrorVariable ScriptError | Select-Object Name, DisplayName, State, ProcessID | Sort-Object State, Name | Format-Table -AutoSize > "$Path\services.txt"
+	Get-WmiObject Win32_Service -ErrorAction SilentlyContinue -ErrorVariable ScriptError | Select-Object Name, DisplayName, State, ProcessID | Sort-Object State, Name | Format-Table -AutoSize | Out-File -FilePath "$Path\services.txt"
 	Write-Log -Message $ScriptError -LogPath $Log
 }
 
@@ -265,13 +280,13 @@ Write-Host "Copying Windows error reports..."
 
 If ( Test-Path -Path "$home\AppData\Local\Microsoft\Windows\WER\ReportArchive" ) {
 
-	Copy-Item -Recurse "$home\AppData\Local\Microsoft\Windows\WER\ReportArchive\*" -Destination "$Path\Error Reports" > $null -ErrorAction SilentlyContinue -ErrorVariable ScriptError
+	Copy-Item "$home\AppData\Local\Microsoft\Windows\WER\ReportArchive\*" -Destination $WER -Recurse -Container 2> $null | Out-Null
 	Write-Log -Message $ScriptError -LogPath $Log
 }
 
 If ( Test-Path -Path "$env:ALLUSERSPROFILE\Microsoft\Windows\WER\ReportArchive" ) {
 
-	Copy-Item -Recurse "$env:ALLUSERSPROFILE\Microsoft\Windows\WER\ReportArchive\*" -Destination "$Path\Error Reports" > $null -ErrorAction SilentlyContinue -ErrorVariable ScriptError
+	Copy-Item "$env:ALLUSERSPROFILE\Microsoft\Windows\WER\ReportArchive\*" -Destination $WER -Recurse -Container 2> $null | Out-Null
 	Write-Log -Message $ScriptError -LogPath $Log
 }
 
